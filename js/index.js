5114e449ce76cb1c68428441c69cb796157e03bb
salambarf@gmail.com <salambarf@gmail.com>
2021-08-22 17:53:28 +0430

    .

--- js/index.js
+++ js/index.js
@@ -1,42 +1,84 @@
-const circleAnimate = document.getElementById("circle-animate");
-const startAnimationBtn = document.getElementById("startBtn");
-const stopAnimationBtn = document.getElementById("stopBtn");
+const slides = document.getElementsByClassName("slide");
+const sliderBox = document.getElementById("slider_box");
+const forDragChange = document.getElementById("forDragChange");
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
+console.log(slides.length);
+let currentKey = 0;
+var animationInterval = setInterval(() => {
+  nextSlide();
+}, 1000);
 
-var id = null;
-var pos = 0;
-
-const startAnimationFunction = () => {
-  clearInterval(id);
-  id = null;
-  if (pos == 500) {
-    pos = 0;
-    circleAnimate.style.left = pos + "px";
+const nextSlide = () => {
+  if (currentKey == slides.length - 1) {
+    slides[currentKey].style.display = "none";
+    currentKey = 0;
+    console.log(currentKey);
+    slides[currentKey].style.display = "block";
+    return;
   }
-  id = setInterval(changePosition, 10);
+  slides[currentKey].style.display = "none";
+  slides[currentKey + 1].style.display = "block";
+  currentKey++;
+  console.log(currentKey);
+  return;
 };
 
-const changePosition = () => {
-  if (pos == 501) {
-    clearInterval(id);
-    id = null;
+const prevSlide = () => {
+  if (currentKey == 0) {
+    slides[currentKey].style.display = "none";
+    currentKey = slides.length - 1;
+    console.log(currentKey);
+    slides[currentKey].style.display = "block";
+    return;
+  }
+  slides[currentKey].style.display = "none";
+  slides[currentKey - 1].style.display = "block";
+  currentKey--;
+  console.log(currentKey);
+  return;
+};
+
+console.log("init");
+const interval = null;
+
+prevBtn.addEventListener("click", prevSlide);
+nextBtn.addEventListener("click", nextSlide);
+
+sliderBox.addEventListener("mouseenter", () => {
+  clearInterval(animationInterval);
+  console.log("cleared");
+});
+
+sliderBox.addEventListener("mouseleave", () => {
+  animationInterval = setInterval(() => {
+    nextSlide();
+  }, 1000);
+  console.log("started");
+});
+
+var dragStartX = 0;
+var dragEndX = 0;
+var dragStartY = 0;
+var dragEndY = 0;
+
+forDragChange.addEventListener("mousedown", function (e) {
+  dragStartX = e.pageX;
+  dragStartY = e.pageY;
+});
+
+forDragChange.addEventListener("mouseup", function (e) {
+  dragEndX = e.pageX - dragStartX;
+  dragEndY = e.pageY - dragStartY;
+
+  console.log("X: " + dragEndX + " Y: " + dragEndY);
+  if (dragEndX < 0) {
+    prevSlide();
   } else {
-    pos += 1.5;
-    circleAnimate.style.left = pos + "px";
+    nextSlide();
   }
-};
+});
 
-const stopAnimationFunction = () => {
-  console.log(typeof id);
-  if (typeof id === "object") {
-    pos = 0;
-    circleAnimate.style.left = pos + "px";
-    clearInterval(id);
-    id = null;
-  } else {
-    clearInterval(id);
-    id = null;
-  }
-};
-
-startAnimationBtn.addEventListener("click", startAnimationFunction);
-stopAnimationBtn.addEventListener("click", stopAnimationFunction);
+forDragChange.addEventListener("click", () => {
+  console.log("click");
+});

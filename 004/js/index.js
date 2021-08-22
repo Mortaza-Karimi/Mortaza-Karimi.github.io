9d429b3d83b75c63f0f2f4a2eda685354e7478b8
salambarf@gmail.com <salambarf@gmail.com>
2021-08-22 20:02:43 +0430

    .

--- 004/js/index.js
+++ 004/js/index.js
@@ -7,9 +7,31 @@ const submit = () => {
   const phoneInput = document.querySelector("#phoneInput");
   const departmentInput = document.querySelector("#departmentInput");
   const messageInput = document.querySelector("#messageInput");
+  const imgInput = document.querySelector("#image");
+  const checkbox1 = document.querySelector("#checkbox1");
+  const checkbox2 = document.querySelector("#checkbox2");
+  const checkbox3 = document.querySelector("#checkbox3");
+  const radio = document.querySelector("#radio");
+
+  const imgSrc = { src: "" };
+
+  imgInput.addEventListener("change", function () {
+    console.log("change");
+    if (this.files && this.files[0]) {
+      // imgInput.onload = () => {
+      //   URL.revokeObjectURL(imgInput.src); // no longer needed, free memory
+      // };
+      console.log("src");
+      imgSrc.src = URL.createObjectURL(this.files[0]);
+
+      // imgInput.src = URL.createObjectURL(this.files[0]); // set src to blob url
+    }
+  });
 
   const formResult =
-    "{<br>'name':'" +
+    "<img src='" +
+    URL.createObjectURL(imgInput.files[0]) +
+    "'><br><br><br><br>{<br>'name':'" +
     nameInput.value +
     "',<br>'phone':'" +
     phoneInput.value +
@@ -17,7 +39,15 @@ const submit = () => {
     departmentInput.value +
     "',<br>'message':'" +
     messageInput.value +
-    "'<br>}";
+    "',<br>'checkbox1':'" +
+    checkbox1.value +
+    "',<br>'checkbox2':'" +
+    checkbox2.value +
+    "',<br>'checkbox3':'" +
+    checkbox3.value +
+    "',<br>'agreement':'" +
+    radio.value +
+    "',<br>}";
   console.log(formResult);
   result.innerHTML = formResult;
   const form = document.getElementById("form");
